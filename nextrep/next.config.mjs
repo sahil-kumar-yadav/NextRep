e080b30bf9dae4cbc0e2564215bc1ff44c2a12be
SAHIL YADAV <121011155+sahil-kumar-yadav@users.noreply.github.com>
2025-07-24 11:29:09 +0000

    update imports

--- nextrep/next.config.mjs
+++ nextrep/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+    serverActions: true, // enables Next.js Server Actions if you're using them
+  },
+};
 
 export default nextConfig;
--- nextrep/src/app/layout.jsx
+++ nextrep/src/app/layout.jsx
@@ -1,6 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Geist, Geist_Mono } from "next/font/google";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
